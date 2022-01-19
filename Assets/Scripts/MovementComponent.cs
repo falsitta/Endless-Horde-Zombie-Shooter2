1efe4eff60a4ed886453833757576d4a07aefbb9
Falsitta <nickfalsitta@gmail.com>
2022-01-19 14:23:22 -0500

    added in player controller, animations, blend tree

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementComponent : MonoBehaviour
+{
+    //movement variables
+    [SerializeField]
+    float walkSpeed = 5;
+    [SerializeField]
+    float runSpeed = 10;
+    [SerializeField]
+    float jumpForce = 5;
+
+    //components
+    PlayerController playerController;
+    Rigidbody rigidbody;
+    Animator playerAnimator;
+
+    // movement references
+    Vector2 inputVector = Vector2.zero;
+    Vector3 moveDirection = Vector3.zero;
+
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("IsJumping");
+    public readonly int isRunningHash = Animator.StringToHash("IsRunning");
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.isJumping) return;
+        if (!(inputVector.magnitude > 0)) moveDirection = Vector3.zero;
+
+        moveDirection = transform.forward * inputVector.y + transform.right * inputVector.x;
+        float currentSpeed = playerController.isRunning ? runSpeed : walkSpeed;
+
+        Vector3 movementDirection = moveDirection * (currentSpeed * Time.deltaTime);
+
+        transform.position += movementDirection;
+
+    }
+
+    public void OnMovement(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
+    }
+
+    public void OnRun(InputValue value)
+    {
+        playerController.isRunning = value.isPressed;
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
+    }
+
+    public void OnJump(InputValue value)
+    {
+        playerController.isJumping = value.isPressed;               
+        rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground") && !playerController.isJumping) return;
+
+        playerController.isJumping = false;
+        playerAnimator.SetBool(isJumpingHash, false);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isFiring;
+    public bool isReloading;
+    public bool isJumping;
+    public bool isRunning;
+}
